312ee8a3355010cfedd959a2ad858ad1701a4bc6
UMAANG <kumaang19@gmail.com>
2023-06-25 18:22:55 +0530

    Added mysql dump directory

---
+++ mysql_dump/trainreservationsystem_pay_info.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: trainreservationsystem
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pay_info`
+--
+
+DROP TABLE IF EXISTS `pay_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pay_info` (
+  `Payment_id` varchar(45) NOT NULL,
+  `Pay_mode` varchar(45) NOT NULL,
+  `Amount` double DEFAULT NULL,
+  `Pay_date` date DEFAULT NULL,
+  `Srl-no` varchar(45) DEFAULT NULL,
+  `PNR-no` varchar(45) NOT NULL,
+  `inst_type` varchar(45) DEFAULT NULL,
+  `inst_amount` double DEFAULT NULL,
+  `Passenger_Id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`Payment_id`),
+  KEY `IdPassenger_idx` (`Passenger_Id`),
+  CONSTRAINT `IdPassenger` FOREIGN KEY (`Passenger_Id`) REFERENCES `pax_info` (`Passenger_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pay_info`
+--
+
+LOCK TABLES `pay_info` WRITE;
+/*!40000 ALTER TABLE `pay_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pay_info` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-06-25 18:22:14
