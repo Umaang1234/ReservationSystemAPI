63bfae60206c6567172b47f580537b8558f99b5b
Shristi1698 <104490423+Shristi1698@users.noreply.github.com>
2023-06-26 00:12:22 +0530

    Added get method to retrieve all passenger info. (#1)

--- src/main/java/com/project/Reservation/dao/ReservationDao.java
+++ src/main/java/com/project/Reservation/dao/ReservationDao.java
@@ -2,6 +2,10 @@ package com.project.Reservation.dao;
 
 import com.project.Reservation.dto.Passenger;
 
+import java.util.List;
+
 public interface ReservationDao {
     void addPassenger(Passenger passenger);
+    List<Passenger> getPassenger();
+
 }
